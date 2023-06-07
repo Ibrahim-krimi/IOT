19075e1de488bd54a6a7e95637c7b2d0d1239009
Dell <ibrahimkrimi2@gmail.com>
2023-06-07 21:10:32 +0200

    3rd Commit

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const service = express();
 const port = 6969;
 
@@ -23,21 +26,21 @@ service.post('/insert', async (req, res) => {
 
 // GET, Récupère les données de la base de données
 service.get('/get', async (req, res) => {
-    try {
-      const client = await MongoClient.connect('mongodb+srv://ibrahim:ibrahim@cluster0.uhijttw.mongodb.net/?retryWrites=true&w=majority');
-      const db = client.db('poolDatabase');
-  
-      const collection = db.collection('pool');
-      const documents = await collection.find().toArray();
-  
-      client.close();
-  
-      res.status(200).json(documents);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+  try {
+    const client = await MongoClient.connect('mongodb+srv://ibrahim:ibrahim@cluster0.uhijttw.mongodb.net/?retryWrites=true&w=majority');
+    const db = client.db('poolDatabase');
+
+    const collection = db.collection('access');
+    const documents = await collection.find().toArray();
+
+    client.close();
+
+    res.status(200).json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 // Lancement du serveur
 service.listen(port, () => {
